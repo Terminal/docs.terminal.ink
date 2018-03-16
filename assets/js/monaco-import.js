ee216095e95dccd596335e8c4cf58030764ebfb7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-16 01:49:27 +0000

    Deploy docs.terminal.ink to github.com/Terminal/docs.terminal.ink.git:gh-pages

---
+++ assets/js/monaco-import.js
@@ -0,0 +1,33 @@
+require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@0.11.1/min/vs/' }});
+
+// Before loading vs/editor/editor.main, define a global MonacoEnvironment that overwrites
+// the default worker url location (used when creating WebWorkers). The problem here is that
+// HTML5 does not allow cross-domain web workers, so we need to proxy the instantiation of
+// a web worker through a same-domain script
+window.MonacoEnvironment = {
+    getWorkerUrl: function(workerId, label) {
+        return '/assets/js/monaco-proxy.js';
+    }
+};
+
+require(['vs/editor/editor.main'], function() {
+    const blocks = document.getElementsByClassName('to-vscode');
+    [...blocks].forEach((block) => {
+        // Create a new editor
+        const div = document.createElement('div');
+        div.classList.add('vscode');
+
+        const editor = monaco.editor.create(div, {
+            value: block.textContent.trim(),
+            language: block.dataset.language || null,
+            scrollBeyondLastLine: false
+        });
+
+        // Destroy the old element and replace with the vscode block
+        block.parentNode.replaceChild(div, block)
+
+        // Resize and show!
+        div.style.height = editor.getScrollHeight() < 500 ? editor.getScrollHeight() : 500;
+        editor.layout();
+    });
+});
---
+++ assets/js/monaco-proxy.js
@@ -0,0 +1,4 @@
+self.MonacoEnvironment = {
+    baseUrl: 'https://unpkg.com/monaco-editor@0.11.1/min/'
+};
+importScripts('https://unpkg.com/monaco-editor@0.11.1/min/vs/base/worker/workerMain.js');
