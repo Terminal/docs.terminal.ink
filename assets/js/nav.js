234092ffb1bd8f8bc1d58eb658e0ab057fc1949c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-06 00:38:54 +0000

    Deploy docs.terminal.ink to github.com/Terminal/docs.terminal.ink.git:gh-pages

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,14 +1,27 @@
-const sidenav = document.getElementById('sidenav');
-const sidedarken = document.getElementById('sidedarken');
+(() => {
+  const sidenav = document.getElementById('sidenav');
+  const sidedarken = document.getElementById('sidedarken');
+  const navRandom = document.getElementById('nav-random');
 
-const toggleNav = () => { // eslint-disable-line no-unused-vars
-	if (sidenav.className === 'sidenavon') {
-		sidenav.className = 'sidenavoff';
-		sidedarken.className = 'sidedarkenoff';
-		document.body.className = 'scroll';
-	} else {
-		sidenav.className = 'sidenavon';
-		sidedarken.className = 'sidedarkenon';
-		document.body.className = 'noscroll';
-	}
-};
+  const data = [
+    'owo what\'s this?',
+    'Hello there!',
+    'UwU it\'s alright',
+    'it is wednesday my dudes'
+  ];
+
+  window.toggleNav = () => { // eslint-disable-line no-unused-vars
+    if (sidenav.className === 'sidenavon') {
+      sidenav.className = 'sidenavoff';
+      sidedarken.className = 'sidedarkenoff';
+      document.body.className = 'scroll';
+    } else {
+      // Generate a random message
+      navRandom.innerHTML = data[Math.floor(Math.random()*data.length)];
+      sidenav.className = 'sidenavon';
+      sidedarken.className = 'sidedarkenon';
+      document.body.className = 'noscroll';
+    }
+  };
+})()
+
